a3e100a309e77ec08312ae2bae034d92d507b524
Andrew Qiao <andrewqiao2004@gmail.com>
2022-09-17 11:09:07 -0400

    Continue landing

--- screens/Landing.js
+++ screens/Landing.js
@@ -1,15 +1,25 @@
-import { StatusBar, SafeAreaView, View, Text, StyleSheet } from "react-native";
+import { SafeAreaView, View, Text, Image, StyleSheet } from "react-native";
 import { COLORS, FONTS } from "../constants/theme";
 
 function LandingScreen() {
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.screen}>
-        <View style={styles.header}>
-          <Text style={styles.heading}>Find first aid nearby.</Text>
+      <View style={styles.screenContainer}>
+        <View>
+          <View style={styles.headerContainer}>
+            <Text style={styles.heading}>Find first aid nearby.</Text>
+          </View>
+          <View style={styles.graphicContainer}>
+            <View style={styles.graphicBackground}>
+              <Image
+                source={require("../assets/img/doctor.png")}
+                style={styles.doctorGraphic}
+              />
+            </View>
+          </View>
         </View>
       </View>
-      <View></View>
+      <View style={styles.buttonsContainer}></View>
     </SafeAreaView>
   );
 }
@@ -18,19 +28,41 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: COLORS.white,
+    justifyContent: "space-between",
   },
-  screen: {
+  screenContainer: {
     marginHorizontal: 20,
     marginTop: 32,
     marginBottom: 48,
   },
-  header: {
-    marginBottom: 20,
+  headerContainer: {
+    marginBottom: 30,
   },
   heading: {
     color: COLORS.black,
     fontFamily: FONTS.bold,
     fontSize: 44,
+  },
+  graphicContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    marginHorizontal: -20,
+  },
+  graphicBackground: {
+    flex: 0,
+    alignItems: "flex-end",
+    justifyContent: "flex-end",
+    width: "85%",
+    height: 260,
+    backgroundColor: COLORS.beige,
+    borderTopLeftRadius: 300,
+    borderBottomLeftRadius: 300,
+  },
+  doctorGraphic: {
+    marginRight: 50,
+    width: 148,
+    height: 223,
   },
 });
 
